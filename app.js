f3720b52336dfc39e54799451e993e861daafce6
crisiito <crisludue5@gmail.com>
2026-05-04 17:49:14 -0300

    carrito de compras v1 javascript

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-const frutas = ["pera", "frutilla"]
-const frutaeliminada = frutas.shift()
-console.log(frutaeliminada) // Output: "pera"
-console.log(frutas) // Output: ["frutilla"]
+const array = []
+
+const frutas = prompt("¿Cuántas frutas quieres agregar?")
+
+array.push(frutas)
+
+while(confirm("¿Quieres agregar otra fruta?")) {
+    const frutas = prompt("¿Qué fruta quieres agregar?")
+    array.push(frutas)
+}
+
+console.log("compraste: " + array)
+
+for(const fruta of array) {
+    console.log(fruta)
+}
+
+
